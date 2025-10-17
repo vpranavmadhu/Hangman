1db43b7512335ffe3a425338a3895017cb82342f
Pranav Madhu <pranavmadhu2003@gmail.com>
2025-10-17 11:13:42 +0530

    RED: Adds test case to check the state of the game if the guess is correct

--- hangman_test.go
+++ hangman_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"strings"
@@ -54,5 +55,34 @@ func TestSecretWordPunctuations(t *testing.T) {
 	secretWord := getSecretWord(wordList)
 	if secretWord != "monkey" {
 		t.Errorf("Should get monkey but Got %s", secretWord)
+	}
+}
+
+func TestCorrectGuess(t *testing.T) {
+	secretWord := "soldier"
+	guess := 's'
+	currentState := newGame(secretWord)
+	newState := checkGuess(currentState, byte(guess))
+
+	expected := Hangman{
+		secretWord:       currentState.secretWord,
+		guesses:          append(currentState.guesses, byte(guess)),
+		correctGuesses:   append(currentState.correctGuesses, byte(guess)),
+		remainingChances: 7,
+	}
+
+	if newState.secretWord != expected.secretWord {
+		t.Errorf("Secret word is modified")
+	}
+	if !bytes.Equal(newState.guesses, expected.guesses) {
+		t.Errorf("Guess should be %q but got %q", expected.guesses, newState.guesses)
+	}
+
+	if !bytes.Equal(newState.correctGuesses, expected.correctGuesses) {
+		t.Errorf("Correct Guess should be %q but got %q", expected.correctGuesses, newState.correctGuesses)
+	}
+
+	if !(newState.remainingChances == expected.remainingChances) {
+		t.Errorf("Remaining chances is modified")
 	}
 }
